c7067ec7a54b61842f1f5c399e7c276bd22ec1b8
Qimat Luo <qimatluo@exosite.com>
2017-01-02 14:52:44 +0800

    Add hand pawn

--- TsumetsumeLoad/play.py
+++ TsumetsumeLoad/play.py
@@ -87,34 +87,54 @@ class TsumeTsumeLoad:
         size = 123
         shiftX = [0, 1, 1, 2, 3]
 
-        for yi, y in enumerate(range(h)):
+        for y in range(h):
             row = []
             for x in range(w):
-                i = sub.getRawPixelInt(x, y)
-                p = sub.getRawPixel(x, y)
-                #print '%s %d %d,%d' %(p, i, x, y)
-                if reduce(lambda a, b: a + b, p[1:]) / len(p[1:]) < 10:
-                    row.append('1')
-                elif p[1] > p[2] and p[1] > p[3] and p[1] > 170 and p[2] > 170 and p[3] > 100: #general
-                    row.append('1')
-                else:
-                    row.append('0')
+                row.append(self.pixelParse(x, y, sub))
 
             if origin == None:
                 try:
                     ''.join(row).index('1000000000000000000000000000') #try this
-                    origin = yi - 1
+                    origin = y - 1
                 except:
                     origin = None
             else:
-                boardY = (yi - origin - 1) / size
+                boardY = (y - origin - 1) / size
                 for boardX in range(5):
                     pos = size * boardX + shiftX[boardX]
                     section = row[pos + 3:pos + size - 3]
                     if len(''.join(section).replace('0', '')):
                         self.board[boardY][boardX].append(section)
-                        
 
+    def setHand(self, img):
+        x = 73
+        y = 1145
+        w = 117
+        h = 117
+        sub = img.getSubImage((x, y, w, h))
+        sub.writeToFile('./sub.png')
+        self.board[4] = [[]]
+
+        for y in range(h):
+            row = []
+
+            for x in range(w):
+                row.append(self.pixelParse(x, y, sub))
+
+            if len(''.join(row).replace('0', '')):
+                self.board[4][0].append(row)
+
+    def pixelParse(self, x, y, sub):
+        i = sub.getRawPixelInt(x, y)
+        p = sub.getRawPixel(x, y)
+        #print '%s %d %d,%d' %(p, i, x, y)
+        if reduce(lambda a, b: a + b, p[1:]) / len(p[1:]) < 10:
+            return '1'
+        elif p[1] > p[2] and p[1] > p[3] and p[1] > 170 and p[2] > 170 and p[3] > 100: #general
+            return '1'
+        else:
+            return '0'
+                        
     def get(self, x, y):
         pawn = self.board[y][x]
         if len(pawn) < 100:
@@ -134,20 +154,20 @@ class TsumeTsumeLoad:
                 xor = int(''.join(pawn[i]), 2) ^ int(''.join(value[i]), 2)
                 error += len(re.sub('[^1]', '', bin(xor)))
 
-            if error < 1500:
+            if error < 1600:
                 return key
 
     def test(self, img):
         self.setBoard(img)
-        for y in range(4):
-            row = []
-            for x in range(5):
+        self.setHand(img)
+        for y, row in enumerate(self.board):
+            for x, col in enumerate(row):
                 parse = self.get(x, y)
                 if parse == None:
-                    call(['mv', '1.png', str(time.time()) + '.png'])
+                    call(['cp', '1.png', str(time.time()) + '.png'])
 
-                row.append(parse)
-            print row
+                print parse, ',',
+            print ''
 
 test = len(sys.argv) > 1
 self = TsumeTsumeLoad(test)
