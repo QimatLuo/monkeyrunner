eca0309532289fa8d1c7f24975e53ca75f0060a0
Qimat Luo <qimatluo@exosite.com>
2017-01-20 22:43:14 +0800

    Bugfix

--- TsumetsumeLoad/play.py
+++ TsumetsumeLoad/play.py
@@ -133,50 +133,12 @@ class TsumeTsumeLoad:
 
         return pawn
 
-    def linearCheck(self, a, b):
-        ax = int(a[0])
-        ay = int(a[1])
-        name = self.board[ay][ax]
-        if not (name == 'hisha' or name == 'ryuo' or name == 'kakugyo' or name == 'ryuma' or name == 'kyosha'):
-            return True
-
-        bx = int(b[0])
-        by = int(b[1])
-        ix = 1 if ax < bx else -1
-        iy = 1 if ay < by else -1
-
-        if ax == bx:
-            loop = True
-            while loop:
-                ay += iy
-
-                if ay == by:
-                    loop = False
-                elif not type(self.board[ay][ax]) is bool:
-                    return False
-        elif ay == by:
-            loop = True
-            while loop:
-                ax += ix
-
-                if ax == bx:
-                    loop = False
-                elif not type(self.board[ay][ax]) is bool:
-                    return False
-        else:
-            loop = True
-            while loop:
-                ax += ix
-                ay += iy
-
-                if ax == bx and ay == by:
-                    loop = False
-                elif not type(self.board[ay][ax]) is bool:
-                    return False
-        return True
-
     def move(self, xy, name, board, cover = False):
         reverse = 1
+        if '_' in name:
+            if not name == 'osho_':
+                reverse = -1
+                name = name[0:-1]
 
         target = [int(xy[0]), int(xy[1])]
         able = []
@@ -211,7 +173,7 @@ class TsumeTsumeLoad:
             i = 1
             pawn = True
             while pawn:
-                x = target[0] + i
+                x = target[0] + i * reverse
                 y = target[1]
                 pawn = self.isAble(x, y, name, board, cover)
                 if pawn:
@@ -228,7 +190,7 @@ class TsumeTsumeLoad:
             i = -1
             pawn = True
             while pawn:
-                x = target[0] + i
+                x = target[0] + i * reverse
                 y = target[1]
                 pawn = self.isAble(x, y, name, board, cover)
                 if pawn:
@@ -246,7 +208,7 @@ class TsumeTsumeLoad:
             pawn = True
             while pawn:
                 x = target[0]
-                y = target[1] + i
+                y = target[1] + i * reverse
                 pawn = self.isAble(x, y, name, board, cover)
                 if pawn:
                     if pawn == 'empty' or pawn == 'osho_':
@@ -263,7 +225,7 @@ class TsumeTsumeLoad:
             pawn = True
             while pawn:
                 x = target[0]
-                y = target[1] + i
+                y = target[1] + i * reverse
                 pawn = self.isAble(x, y, name, board, cover)
                 if pawn:
                     if pawn == 'empty' or pawn == 'osho_':
@@ -282,8 +244,8 @@ class TsumeTsumeLoad:
             j = 1
             pawn = True
             while pawn:
-                x = target[0] + i
-                y = target[1] + j
+                x = target[0] + i * reverse
+                y = target[1] + j * reverse
                 pawn = self.isAble(x, y, name, board, cover)
                 if pawn:
                     if pawn == 'empty' or pawn == 'osho_':
@@ -301,8 +263,8 @@ class TsumeTsumeLoad:
             j = -1
             pawn = True
             while pawn:
-                x = target[0] + i
-                y = target[1] + j
+                x = target[0] + i * reverse
+                y = target[1] + j * reverse
                 pawn = self.isAble(x, y, name, board, cover)
                 if pawn:
                     if pawn == 'empty' or pawn == 'osho_':
@@ -316,13 +278,12 @@ class TsumeTsumeLoad:
                         else:
                             able.append(str(x) + str(y))
                         pawn = False
-                    pawn = False
             i = 1
             j = -1
             pawn = True
             while pawn:
-                x = target[0] + i
-                y = target[1] + j
+                x = target[0] + i * reverse
+                y = target[1] + j * reverse
                 pawn = self.isAble(x, y, name, board, cover)
                 if pawn:
                     if pawn == 'empty' or pawn == 'osho_':
@@ -336,13 +297,12 @@ class TsumeTsumeLoad:
                         else:
                             able.append(str(x) + str(y))
                         pawn = False
-                    pawn = False
             i = -1
             j = 1
             pawn = True
             while pawn:
-                x = target[0] + i
-                y = target[1] + j
+                x = target[0] + i * reverse
+                y = target[1] + j * reverse
                 pawn = self.isAble(x, y, name, board, cover)
                 if pawn:
                     if pawn == 'empty' or pawn == 'osho_':
@@ -356,7 +316,6 @@ class TsumeTsumeLoad:
                         else:
                             able.append(str(x) + str(y))
                         pawn = False
-                    pawn = False
             return able
         elif name == 'keima':
             possible = [
@@ -377,7 +336,7 @@ class TsumeTsumeLoad:
             pawn = True
             while pawn:
                 x = target[0]
-                y = target[1] + i
+                y = target[1] + i * reverse
                 pawn = self.isAble(x, y, name, board, cover)
                 if pawn:
                     if pawn == 'empty' or pawn == 'osho_':
@@ -396,8 +355,8 @@ class TsumeTsumeLoad:
             j = 1
             pawn = True
             while pawn:
-                x = target[0] + i
-                y = target[1] + j
+                x = target[0] + i * reverse
+                y = target[1] + j * reverse
                 pawn = self.isAble(x, y, name, board, cover)
                 if pawn:
                     if pawn == 'empty' or pawn == 'osho_':
@@ -411,13 +370,12 @@ class TsumeTsumeLoad:
                         else:
                             able.append(str(x) + str(y))
                         pawn = False
-                    pawn = False
             i = -1
             j = -1
             pawn = True
             while pawn:
-                x = target[0] + i
-                y = target[1] + j
+                x = target[0] + i * reverse
+                y = target[1] + j * reverse
                 pawn = self.isAble(x, y, name, board, cover)
                 if pawn:
                     if pawn == 'empty' or pawn == 'osho_':
@@ -431,13 +389,12 @@ class TsumeTsumeLoad:
                         else:
                             able.append(str(x) + str(y))
                         pawn = False
-                    pawn = False
             i = 1
             j = -1
             pawn = True
             while pawn:
-                x = target[0] + i
-                y = target[1] + j
+                x = target[0] + i * reverse
+                y = target[1] + j * reverse
                 pawn = self.isAble(x, y, name, board, cover)
                 if pawn:
                     if pawn == 'empty' or pawn == 'osho_':
@@ -451,13 +408,12 @@ class TsumeTsumeLoad:
                         else:
                             able.append(str(x) + str(y))
                         pawn = False
-                    pawn = False
             i = -1
             j = 1
             pawn = True
             while pawn:
-                x = target[0] + i
-                y = target[1] + j
+                x = target[0] + i * reverse
+                y = target[1] + j * reverse
                 pawn = self.isAble(x, y, name, board, cover)
                 if pawn:
                     if pawn == 'empty' or pawn == 'osho_':
@@ -471,7 +427,6 @@ class TsumeTsumeLoad:
                         else:
                             able.append(str(x) + str(y))
                         pawn = False
-                    pawn = False
             possible = [
                 [-1, 0],
                 [0, -1],
@@ -482,7 +437,7 @@ class TsumeTsumeLoad:
             i = 1
             pawn = True
             while pawn:
-                x = target[0] + i
+                x = target[0] + i * reverse
                 y = target[1]
                 pawn = self.isAble(x, y, name, board, cover)
                 if pawn:
@@ -499,7 +454,7 @@ class TsumeTsumeLoad:
             i = -1
             pawn = True
             while pawn:
-                x = target[0] + i
+                x = target[0] + i * reverse
                 y = target[1]
                 pawn = self.isAble(x, y, name, board, cover)
                 if pawn:
@@ -517,7 +472,7 @@ class TsumeTsumeLoad:
             pawn = True
             while pawn:
                 x = target[0]
-                y = target[1] + i
+                y = target[1] + i * reverse
                 pawn = self.isAble(x, y, name, board, cover)
                 if pawn:
                     if pawn == 'empty' or pawn == 'osho_':
@@ -534,7 +489,7 @@ class TsumeTsumeLoad:
             pawn = True
             while pawn:
                 x = target[0]
-                y = target[1] + i
+                y = target[1] + i * reverse
                 pawn = self.isAble(x, y, name, board, cover)
                 if pawn:
                     if pawn == 'empty' or pawn == 'osho_':
@@ -591,7 +546,7 @@ class TsumeTsumeLoad:
                         self.opponent.append(str(x) + str(y))
 
         if error == 1:
-            call(['cp', '1.png', str(time.time()) + '.png'])
+            call(['cp', '1.png', 'parse_' + time.strftime('%d_%H:%M:%S') + '.png'])
         else:
             return True
 
@@ -704,57 +659,122 @@ class TsumeTsumeLoad:
             xor = set(xor) & set(able) ^ set(xor)
             print obj['name'], able
             print xor
-        if not len(xor):
-            return True
+        if len(xor):
+            return False
+
+        return True
+
+    def possibleTeam(self):
+        if type(self.board[4][0]) is bool:
+            array = self.team
+        else:
+            array = ['04']
+        return array
+
+    def checkmate(self, oxy, txy):
+        self.util.click(oxy)
+        self.util.click(txy)
+        self.util.click('upgrade')
+
+    def checkOsho(self, oxy, txy, board):
+        print 'txy should in team cover:'
+        if txy in self.move(self.osho, 'osho_', self.board):
+            for xy in self.team:
+                x = int(xy[0])
+                y = int(xy[1])
+                name = self.board[y][x]
+                if xy == oxy:
+                    continue
+                cover = self.move(xy, name, board, True)
+                print name, cover
+                if (txy in cover):
+                    return True
+        else:
+            tx = int(txy[0])
+            ty = int(txy[1])
+            name = board[ty][tx]
+            if name == 'hisha' or name == 'ryuo' or name == 'kakugyo' or name == 'ryuma' or name == 'kyosha':
+                return False
+            else:
+                return True
+
+    def checkOpponents(self, oxy, txy, board):
+        print 'txy dead should cover osho:'
+        tx = int(txy[0])
+        ty = int(txy[1])
+        for xy in self.opponent:
+            x = int(xy[0])
+            y = int(xy[1])
+            name = self.board[y][x]
+            if txy in self.move(xy, name, self.board):
+                newBoard = copy.deepcopy(board)
+                newBoard[y][x] = False
+                newBoard[ty][tx] = name
+                for xy in self.team:
+                    x = int(xy[0])
+                    y = int(xy[1])
+                    name = self.board[y][x]
+                    if xy == oxy:
+                        continue
+                    cover = self.move(xy, name, newBoard, True)
+                    print name, cover
+                    if (self.osho in cover):
+                        return True
+        return True
 
     def test(self, img):
-        if self.parsePawn(img):
-            self.util.click('close pause')
-            hand = self.board[4][0]
-            if type(hand) is bool:
-                array = self.team
-            else:
-                array = ['04']
+        if not self.parsePawn(img):
+            return False
 
-            for oxy in array:
-                ox = int(oxy[0])
-                oy = int(oxy[1])
-                oname = self.board[oy][ox]
-                able = self.move(oxy, oname, self.board)
+        self.util.click('close pause')
 
-                for txy in able:
-                    tx = int(txy[0])
-                    ty = int(txy[1])
-                    board = copy.deepcopy(self.board)
-                    if oxy == '04':
-                        board[ty][tx] = oname
-                    else:
-                        board[ty][tx] = self.upgrade(oxy, txy, oname)
-                    board[oy][ox] = False
-                    print oxy,'to', txy
-                    print board
-                    if self.getAns(board):
-                        for xy in self.team:
-                            x = int(xy[0])
-                            y = int(xy[1])
-                            name = self.board[y][x]
-                            if xy == oxy:
-                                continue
-                            cover = self.move(xy, name, board, True)
-                            print name, cover
-                            if txy in cover:
-                                self.util.click(oxy)
-                                self.util.click(txy)
-                                self.util.click('upgrade')
-                                return True
+        for oxy in self.possibleTeam():
+            ox = int(oxy[0])
+            oy = int(oxy[1])
+            oname = self.board[oy][ox]
+
+            for txy in self.move(oxy, oname, self.board):
+                print oxy, 'to', txy
+                tx = int(txy[0])
+                ty = int(txy[1])
+                if not type(self.board[ty][tx]) is bool:
+                    continue
+                board = copy.deepcopy(self.board)
+                if oxy == '04':
+                    board[ty][tx] = oname
+                else:
+                    board[ty][tx] = self.upgrade(oxy, txy, oname)
+                board[oy][ox] = False
+
+                if not self.osho in self.move(txy, board[ty][tx], board):
+                    continue
+
+                print board
+
+                if not self.getAns(board):
+                    continue
+
+                if not self.checkOsho(oxy, txy, board):
+                    continue
+
+                if not self.checkOpponents(oxy, txy, board):
+                    continue
+
+                self.checkmate(oxy, txy)
+                return True
+        call(['cp', '1.png', 'bug_' + time.strftime('%d_%H:%M:%S') + '.png'])
                         
     def upgrade(self, oxy, txy, name):
         y1 = int(oxy[1])
         y2 = int(txy[1])
         able = False
         for x in range(5):
-            able = able or self.board[y1][x]
-            able = able or self.board[y2][x]
+            if (not y1 == 4) and (self.board[y1][x] == True):
+                able = True
+                break
+            if (not y2 == 4) and (self.board[y2][x] == True):
+                able = True
+                break
 
         if not able or name == 'ryuo' or name == 'ryuma':
             return name
@@ -781,22 +801,25 @@ else:
     play = 0
     stage = 1
     lose = 0
+    combo = 0
     while  current == self.activity:
         print 'takeSnapshot...'
         img = self.device.takeSnapshot()
         if self.util.pixel('pause', img):
             self.util.click('pause')
-            img.writeToFile('./1.png')
+            img.writeToFile('1.png')
             if self.test(img):
                 play += 1
+                print 'combo:', combo
+                combo += 1
                 self.util.sleep(6)
             else:
-                img.writeToFile('./2.png')
                 self.util.sleep(1)
             self.reset()
         elif self.util.pixel('close pause', img):
             self.util.click('close pause')
         elif self.util.pixel('done', img):
+            self.util.click('done', 2)
             self.util.click('done')
         elif self.util.pixel('done', 'done disabled', img):
             self.util.click('no')
@@ -807,7 +830,7 @@ else:
             lose += 1
             self.util.click('lose')
             self.util.click('lose ok')
-            img.writeToFile('./' + time.strftime('%d_%H:%M:%S') + '.png')
+            img.writeToFile('lose_' + time.strftime('%d_%H:%M:%S') + '.png')
         elif self.util.pixel('stage', img):
             stage += 1
             self.util.click('step1', 2)
@@ -816,6 +839,7 @@ else:
             self.util.click('step3', 2)
             self.util.click('step4', 2)
             self.util.click('step5', 2)
+            combo = 0
 
         current = self.device.getProperty('am.current.comp.class')
     end = time.time()
